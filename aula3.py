dd9ce932d6557637ec9c3074921fb11c409efa49
CavMCarolina <cavm.carolina@gmail.com>
2025-08-22 10:46:05 -0300

    mudando estrutura para usar o mesmos dados em mais de uma aula

---
+++ aula3.py
@@ -0,0 +1,9 @@
+# pip install pandas numpy matplotlib openpyxl
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.linspace(-10, 10, 100)
+B = np.linspace(-5, 5, 100)
+
